be0759d378a6fee2a42c3c53266661abe7922279
Merlijn18 <merlijnvannistelrooij@gmail.com>
2024-11-01 12:24:44 +0100

    s

--- obj/Debug/net8.0/C3 Schoolvoetbal.AssemblyInfo.cs
+++ obj/Debug/net8.0/C3 Schoolvoetbal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C3 Schoolvoetbal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aac48fbda91612b8ed7b888679cef6aa1d62d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("C3 Schoolvoetbal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C3 Schoolvoetbal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
